5b6213a3b36a4eb2bbf0013f4e5da210195537d5
allisonlcching <allisonlcccc@gmail.com>
2018-02-15 23:08:59 -0700

    2nd

---
+++ wp-content/plugins/bp-custom.php
@@ -0,0 +1,7 @@
+<?php	
+	define ( 'BP_AVATAR_THUMB_WIDTH', 50 );
+	define ( 'BP_AVATAR_THUMB_HEIGHT', 50 );
+	define ( 'BP_AVATAR_FULL_WIDTH', 150 );
+	define ( 'BP_AVATAR_FULL_HEIGHT', 150 );
+	define ( 'BP_AVATAR_ORIGINAL_MAX_WIDTH', 800 );
+?>
--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -4,53 +4,13 @@
 	    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 	}
+// https://buddypress.org/support/topic/how-can-i-disable-secondary-avatars-in-activity-items/
+	function my_remove_secondary_avatars( $bp_legacy ) {
+    	remove_filter( 'bp_get_activity_action_pre_meta', array( $bp_legacy, 'secondary_avatars' ), 10, 2 );
+	}
+	add_action( 'bp_theme_compat_actions', 'my_remove_secondary_avatars' );
 ?>
-<?php 
-	function dbftextlogo_set_up_buffer(){
-	    if ( is_feed() || is_admin() ){ return; }
-		try { 
-			if (ini_get('output_buffering')) {
-				ob_start('dbftextlogo_filter_page');
-			}
-		} catch (Exception $e) { }
-	}
-	add_action('wp', 'dbftextlogo_set_up_buffer', 10, 0);
-	 
-	function dbftextlogo_filter_page($content){
-		$title = esc_html(get_bloginfo('name'));
-		$tagline = esc_html(get_bloginfo('description'));
-		$content = preg_replace('#(<img.*id="logo".*/>)#U','\\1<h1 id="logo-text">'.$title.'</h1> <h5 id="logo-tagline">'.$tagline.'</h5>', $content); 
-	    return $content;
-	}
 
-	function dbftextlogo_wp_head() { ?>
-	<style>
-	#logo { padding-right: 10px; }
-	#logo-text, #logo-tagline { 
-		margin:0; 
-		padding:0; 
-		display:inline;
-		vertical-align: middle;
-	}
-	#logo-tagline { opacity: 0.7; margin-left: 16px; vertical-align: sub; }
-	@media only screen and (max-width: 767px) { 
-		#logo-tagline { display: none; }
-	}
-	.et_hide_primary_logo .logo_container { height: 100% !important; opacity: 1 !important; }
-	.et_hide_primary_logo .logo_container #logo { display: none; }
-
-	</style>
-	<script>
-	jQuery(function($) {
-		if (!$('#logo-text').length) {
-			$('#logo').after('<h1 id="logo-text"><?php esc_html_e(get_bloginfo('name')); ?></h1> <h5 id="logo-tagline"><?php esc_html_e(get_bloginfo('description')); ?></h5>');
-		}
-	});
-	</script>
-	<?php
-	}
-	add_action('wp_head', 'dbftextlogo_wp_head');
-?>
 
 
 
